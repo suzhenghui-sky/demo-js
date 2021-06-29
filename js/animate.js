b557907a5be0460f7d4c09c05461cdf40bad8d49
suzhenghui-sky <jyjyjy28@126.com>
2021-06-29 14:49:34 +0800

    完成轮播图全部功能

--- js/animate.js
+++ js/animate.js
@@ -1,17 +1,37 @@
 function animateFirst(node, targetNum, callback) {
-  let step = (targetNum -  node.offsetLeft) / 10;
+  let step = (targetNum - node.offsetLeft) / 6;
   step = step > 0 ? Math.ceil(step) : Math.floor(step);
   node.style.left = node.offsetLeft + step + 'px';
 
   if (node.offsetLeft > targetNum) {
     setTimeout(animateFirst, 30, node, targetNum, callback);
-  }
-  else if (node.offsetLeft < targetNum) {
+  } else if (node.offsetLeft < targetNum) {
     setTimeout(animateFirst, 30, node, targetNum, callback);
-  }
-  else {
+  } else {
     callback && callback();
   }
 }
 
-function animateSecond() {}
+// 有问题；感觉这个方法不好
+// function animateSecond(node, targetNum, callback) {
+//   console.log('xxxx');
+//   let time = 300; // 位移总时间
+//   let interval = 30; // 唯一间隔时间
+//   let speed = targetNum / (time / interval); // 每次移动的偏移量
+//   speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+
+//   function go() {
+//     node.style.left = node.offsetLeft + speed + 'px';
+//     if (node.offsetLeft > targetNum) {
+//       setTimeout(go, 30, node, targetNum, callback);
+//     } else if (node.offsetLeft < targetNum) {
+//       setTimeout(go, 30, node, targetNum, callback);
+//     } else {
+//       callback && callback();
+//     }
+//   }
+
+//   go();
+
+
+// }
--- js/demo.js
+++ js/demo.js
@@ -6,3 +6,11 @@ function querySelector(type_str) {
 function createEle(ele_str) {
   return document.createElement(ele_str);
 }
+
+// 处理小点点
+function currentDot(ele, index) {
+  let list = ele.children;
+  list = [...list];
+  list.forEach(item => item.classList.remove('current'));
+  list[index].classList.add('current');
+}
